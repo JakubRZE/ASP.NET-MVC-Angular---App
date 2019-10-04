8ee7667582aa32b31488e30ebc99d8dc6221ee4a
Jakub Rzepka <Jakubjakk@gmail.com>
2019-10-04 18:16:32 +0200

    Views update

--- ASP.NET MVC App/Controllers/HomeController.cs
+++ ASP.NET MVC App/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace ASP.NET_MVC_App.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ ASP.NET MVC App/Scripts/out-tsc/app/app.module.js
@@ -0,0 +1,22 @@
+import * as tslib_1 from "tslib";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+let AppModule = class AppModule {
+};
+AppModule = tslib_1.__decorate([
+    NgModule({
+        declarations: [
+            AppComponent,
+            ProductListComponent
+        ],
+        imports: [
+            BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+    })
+], AppModule);
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
